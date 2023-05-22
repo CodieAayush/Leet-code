499cfabd55b85ffef05735ee4012ad23791b1438
Aayush Chaudhary <120035320+CodieAayush@users.noreply.github.com>
2023-05-22 22:29:55 +0530

    Create 88. Merge Sorted Array.java

---
+++ Array/88. Merge Sorted Array.java
@@ -0,0 +1,16 @@
+class Solution {
+    public void merge(int[] nums1, int m, int[] nums2, int n) {
+       
+        int c=nums2.length;
+        for(int i=nums1.length-1;i>(nums1.length-nums2.length)-1;i--)
+        {
+            nums1[i]=nums2[--c];
+            //c--;
+            
+        }
+        Arrays.sort(nums1);
+
+        
+    }
+
+}
