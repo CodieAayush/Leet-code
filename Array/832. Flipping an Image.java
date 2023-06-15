600415018a9bfafe5ad926240b836d2aa3e68279
Aayush Chaudhary <120035320+CodieAayush@users.noreply.github.com>
2023-06-15 22:08:27 +0530

    Create 832. Flipping an Image.java

---
+++ Array/832. Flipping an Image.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int[][] flipAndInvertImage(int[][] image) {
+        
+   for(int i=0;i<image.length;i++){
+
+       for(int j=0;j<(image[i].length)/2;j++){
+           int temp=image[i][j];
+           image[i][j]=image[i][image[i].length-1-j];
+           image[i][image[i].length-1-j]=temp;
+       }
+   }
+   for(int i=0;i<image.length;i++){
+       for(int j=0;j<image[i].length;j++){
+           if(image[i][j]==0){
+               image[i][j]=1;
+           }
+           else{
+               image[i][j]=0;
+           }
+       }
+   }
+   return image;
+}}
