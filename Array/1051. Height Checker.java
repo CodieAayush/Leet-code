d90f4223d1996ec250566304e6b8929448832d98
Aayush Chaudhary <120035320+CodieAayush@users.noreply.github.com>
2023-08-29 22:52:37 +0530

    Create 1051. Height Checker.java

---
+++ Array/1051. Height Checker.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int heightChecker(int[] heights) {
+        int count = 0;
+        int expected[] = new int[heights.length];
+        for(int i=0; i<heights.length; i++)
+            expected[i] = heights[i];
+        Arrays.sort(expected);
+        for(int i=0; i<heights.length; i++){
+            if(heights[i] != expected[i])
+                count++;
+        }
+        return count;
+    }
+}
