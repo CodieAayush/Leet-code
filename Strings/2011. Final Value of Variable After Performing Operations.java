35441d30b058786572697aaadf667dd4f4334922
Aayush Chaudhary <120035320+CodieAayush@users.noreply.github.com>
2023-09-05 22:22:35 +0530

    Create 2011. Final Value of Variable After Performing Operations.java

---
+++ Strings/2011. Final Value of Variable After Performing Operations.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int finalValueAfterOperations(String[] operations) {
+        int x=0;
+        for(int i=0;i<operations.length;i++)
+        {
+            if((operations[i].charAt(1))=='-')
+            x=x-1;
+            else 
+            x=x+1;
+          //  else continue;
+        }   
+        return x;     
+    }
+}
